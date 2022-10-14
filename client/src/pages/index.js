c359a590cd817774699e4e169801b71893c55cb3
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-14 11:49:27 +0300

    Cart, Login, Register pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Cart from './Cart';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import ProductList from './ProductList';
+import Product from './Product';
+
+export { Cart, Home, Login, Register, ProductList, Product };
