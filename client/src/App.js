704240fd4962f7904717c7da43cbfc45778e7f12
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-14 11:57:23 +0300

    react-router-dom apply

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import { Home, Login, Register, ProductList, Product, Cart } from './pages';
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/products' element={<ProductList />} />
+          <Route path='/products/:id' element={<Product />} />
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
