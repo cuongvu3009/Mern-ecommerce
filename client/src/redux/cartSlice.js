510758e1744b2879ec34f7ac0c15b9dde715cbe3
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-15 18:40:06 +0300

    redux setup and cartSlice

---
+++ client/src/redux/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  products: [],
+  quantity: 0,
+  total: 0,
+  error: false,
+  loading: false,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addProducts: (state, action) => {
+      state.quantity += 1;
+      state.products.push(action.payload.product);
+      state.total += action.payload.price;
+    },
+  },
+});
+
+export const { addProducts } = cartSlice.actions;
+export default cartSlice.reducer;
